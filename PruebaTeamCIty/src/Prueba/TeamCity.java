836daf5c82099c9375be4d553ebdafc5e3a78748
Dayaneth Arrocha <dayamar0409@gmail.com>
2020-06-07 19:44:05 -0500

    Probando y editando

--- PruebaTeamCIty/src/Prueba/TeamCity.java
+++ PruebaTeamCIty/src/Prueba/TeamCity.java
@@ -3,7 +3,7 @@ package Prueba;
 public class TeamCity {
 
 	public static void main(String[] args) {
-		System.out.println("¿Quié hizo la animación completa del muñeco de jengibre?");
+		System.out.println("Ahora se formó la pelea");
 	}
 
 }
