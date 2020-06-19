18e32b8882e9c506a23700202ff828bc681566cb
Dayaneth Arrocha <Dayaneth Arrocha@DESKTOP-ACILH0L>
2020-06-18 22:58:23 -0500

    Pruebas

--- PruebaTeam/src/Prueba.java
+++ PruebaTeam/src/Prueba.java
@@ -1,7 +1,6 @@
 
 public class Prueba {
 	public static void main(String[] args) {
-		System.out.println("Ingienería de Software II.");
-		System.out.println("Taller 2 - Integración continua");
+		System.out.println("Taller 2 - Integración Continua");
 	}
 }
