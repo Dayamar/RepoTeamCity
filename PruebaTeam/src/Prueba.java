61084128321757c77420a2474c66c69bf71e7f34
Dell <Dell@DESKTOP-ACILH0L>
2020-06-18 21:09:00 -0500

    Haciendo cambios

--- PruebaTeam/src/Prueba.java
+++ PruebaTeam/src/Prueba.java
@@ -1,7 +1,7 @@
 
 public class Prueba {
 	public static void main(String[] args) {
-		System.out.println("Ingienería de Software II");
+		System.out.println("Ingienería de Software II.");
 		System.out.println("Taller 2 - Integración continua");
 	}
 }
